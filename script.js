dd6decedaf236213ce088a1550b3d9b2228439e5
Hirak Naskar <hiraknaskar15203@gmail.com>
2024-07-21 01:56:37 +0530

    page4done

--- script.js
+++ script.js
@@ -35,18 +35,18 @@ function LoadingAnimation() {
     y: 1600,
     opacity: 0,
     duration: 0.6,
-    ease: "power4",
+    ease: "power4.out",
   });
   tl.to("#loader", {
     display: "none",
   });
   tl.from("#nav",{
     opacity:0,
-})
+});
   tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
-    y:120,
+    y:140,
     stagger:0.2
-})
+});
 }
 
 
